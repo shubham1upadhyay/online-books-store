466670d74633c4f1c53334467ab2df58ac39238a
shubham1upadhyay <shubhamup47@gmail.com>
2023-08-26 23:59:55 +0530

    code updated

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -14,6 +14,7 @@ const BookDetails = ({ bookInfo, setBookList, selectedBook, showInfo }) => {
     else {
       getBooksData(bookInfo);
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [bookInfo])
 
 
